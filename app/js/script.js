e4e58c306ee3a1b0e8ea37f4a1207d7bf63cf8be
Simit-Yadav <sim5394yadav@hgmail.com>
2021-06-24 21:39:23 -0400

    final upload

--- app/js/script.js
+++ app/js/script.js
@@ -1,28 +1,24 @@
-const btnHamburger = document.querySelector('#btnHamburger');
-const body = document.querySelector('body');
-const header = document.querySelector('.header');
-const overlay = document.querySelector('.overlay');
-const fadeElems = document.querySelectorAll('.has-fade');
+const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
 
-btnHamburger.addEventListener('click', function(){
-  console.log('click hamburger');
-
-  if(header.classList.contains('open')){ // Close Hamburger Menu
-    body.classList.remove('noscroll');
-    header.classList.remove('open');    
-    fadeElems.forEach(function(element){
-      element.classList.remove('fade-in');
-      element.classList.add('fade-out');
+header.addEventListener("click", () => {
+  console.log("open hamburger");
+  if (header.classList.contains("open")) {
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeElems.forEach((elem) => {
+      elem.classList.remove("fade-in");
+      elem.classList.add("fade-out");
     });
-    
+  } else {
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeElems.forEach((elem) => {
+      elem.classList.remove("fade-out");
+      elem.classList.add("fade-in");
+    });
   }
-  else { // Open Hamburger Menu
-    body.classList.add('noscroll');
-    header.classList.add('open');
-    fadeElems.forEach(function(element){
-      element.classList.remove('fade-out');
-      element.classList.add('fade-in');
-    });
-
-  }  
 });
